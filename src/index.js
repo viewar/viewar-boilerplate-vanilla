058505352aa11b2b374fbae096f14c694bbec75d
Gernot Stangl <gs@viewar.com>
2019-07-05 18:09:16 +0200

    reverted changes

--- src/index.js
+++ src/index.js
@@ -6,28 +6,21 @@ import './index.scss';
 (async function main() {
   window.api = await viewarApi.init();
 
-  const testModel = await viewarApi.modelManager.fetchModelFromRepository(
-    '53696' // configurationModel 53696
+  const sheepModel = await viewarApi.modelManager.fetchModelFromRepository(
+    '20'
   );
 
-  const instance = await viewarApi.sceneManager.insertModel(testModel, {
-    pose: {
-      position: {
-        x: 0,
-        y: 0,
-        z: 0,
+  for (let x = 0; x < 20; ++x) {
+    await viewarApi.sceneManager.insertModel(sheepModel, {
+      pose: {
+        position: {
+          x: Math.random() * 4000 - 2000,
+          y: 0,
+          z: Math.random() * 4000 - 2000,
+        },
       },
-    },
-  });
-
-  // default propertyValues for model with modelId 38388
-  // {Chrome: "chrome", Cushion: "black", Coating: "01RAL9016weiss", Wood: "01WoodFine0014_L"}
-  // Cushion: "white" is valid, Cushion: "red" is invalid
-
-  // setTimeout(() => instance.setPropertyValues({Chrome: "chrome", Cushion: "white", Coating: "01RAL9016weiss", Wood: "01WoodFine0014_L"}, 2000));
-  
-  // use this if you test with modelId 53696
-  setTimeout(() => instance.setPropertyValues({['Verkleidung Links']: "Speckstein"}), 2000);
+    });
+  }
 
   console.log('log', {
     'a': 1234,
@@ -37,4 +30,3 @@ import './index.scss';
   console.warn('warn');
   console.error('error');
 })();
-
